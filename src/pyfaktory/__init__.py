f439da1037ded2095bf91a79ecdf82a9fd3b3c4e
Ghiles Meddour <ghiles.meddour.b@gmail.com>
2021-09-05 23:46:13 +0200

    add init file

---
+++ src/pyfaktory/__init__.py
@@ -0,0 +1,7 @@
+from .client import Client
+from .consumer import Consumer
+from .producer import Producer
+from .util.exceptions import *
+
+__version__ = "0.0.0"
+__url__ = "https://github.com/ghilesmeddour/faktory_worker_python"
