51ac41e6348931fd0aa5ee799d317780eceec6a2
Ghiles Meddour <ghiles.meddour.b@gmail.com>
2021-09-05 23:48:02 +0200

    add consumer draft source

---
+++ src/pyfaktory/consumer.py
@@ -0,0 +1,60 @@
+from concurrent.futures import ProcessPoolExecutor
+from typing import Callable, Dict, List
+import logging
+
+from .client import Client
+from .util.enums import LifecycleEventType
+from .util import constants as C
+
+
+class Consumer:
+    """
+    Faktory Consumer (Worker).
+
+    Parameters
+    ----------
+    client : pyfaktory.Client
+        Faktory Client.
+    concurrency : int, default 1
+        Number of processes to start.
+    beat_period : int, default 15
+        Lorem.
+    """
+    def __init__(self,
+                 client: Client,
+                 concurrency: int = 1,
+                 beat_period=C.RECOMMENDED_BEAT_PERIOD) -> None:
+        self.client = client
+        self.concurrency = concurrency
+        self.beat_period = beat_period
+        self.executor = ProcessPoolExecutor(max_workers=self.concurrency)
+
+        self.logger = logging.getLogger(name='FaktoryConsumer')
+
+        self.labels = []
+        self.pool = None
+        self.state = ''
+        self.queues = []
+        self.done = None
+        self.shutdown_waiter = None
+        self.job_handlers: Dict[str, Callable] = {}
+        self.event_handlers: Dict[LifecycleEventType, List[Callable]] = None
+        self.weighted_priority_queues_enabled: bool = False
+        self.weighted_queues = None
+        pass
+
+    def register(self, name: str, fn: Callable):
+        """
+        Register a handler for the given jobtype.
+
+        It is expected that all jobtypes are registered upon process
+        startup.
+        """
+        self.job_handlers[name] = fn
+        self.logger.info(f"Registered handler for jobtype: {name}")
+
+    def run(self):
+        """
+        """
+        while True:
+            pass
