9e4c4a92d69ab5a34a1bd03ed8e5dbe19e8d3428
Ghiles Meddour <ghiles.meddour.b@gmail.com>
2021-09-08 23:02:22 +0200

    consumer: update __init__

--- src/pyfaktory/consumer.py
+++ src/pyfaktory/consumer.py
@@ -32,26 +32,47 @@ class Consumer:
     """
     def __init__(self,
                  client: Client,
-                 concurrency: int = 1,
-                 beat_period=C.RECOMMENDED_BEAT_PERIOD) -> None:
-        self.client = client
-        self.concurrency = concurrency
-        self.beat_period = beat_period
-        self.executor = ProcessPoolExecutor(max_workers=self.concurrency)
-
+                 queues: List[str] = C.DEFAULT_QUEUES,
+                 priority: str = 'uniform',
+                 weights: Optional[List[float]] = None,
+                 concurrency: int = 4,
+                 grace_period: int = C.DEFAULT_GRACE_PERIOD) -> None:
         self.logger = logging.getLogger(name='FaktoryConsumer')
 
-        self.labels = []
-        self.pool = None
-        self.state = ''
-        self.queues = []
-        self.done = None
-        self.shutdown_waiter = None
+        if client.role == 'producer':
+            raise Exception(
+                "Provided client is exclusively producer and can't act as a consumer"
+            )
+        self.client = client
+
+        self.queues = queues
+
+        if priority not in ['strict', 'uniform', 'weighted']:
+            raise ValueError(
+                f"Unexpected priority ({priority}), priority should be 'strict', 'uniform' or 'weighted'"
+            )
+        self.priority = priority
+
+        if self.priority == 'weighted':
+            if weights is None:
+                raise ValueError(
+                    'Priority is weighted but weights are not provided')
+            elif len(weights) != len(self.queues):
+                raise ValueError('Weights and queues lengths mismatch')
+            elif abs(math.fsum(weights) - 1.) > 0.0001:
+                raise ValueError('Weights do not sum to 1')
+            else:
+                self.weights = weights
+
+        self.concurrency = concurrency
+        self.grace_period = grace_period
+        
+        self.pool = ProcessPool(max_workers=self.concurrency)
+        
         self.job_handlers: Dict[str, Callable] = {}
-        self.event_handlers: Dict[LifecycleEventType, List[Callable]] = None
-        self.weighted_priority_queues_enabled: bool = False
-        self.weighted_queues = None
-        pass
+
+        self.pending_tasks_count = 0
+        self.lock_pending_tasks_count = multiprocessing.Lock()
 
     def register(self, name: str, fn: Callable):
         """
