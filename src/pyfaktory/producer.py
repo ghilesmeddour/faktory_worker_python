9fe3d74fd5cb3ff3483b65e9ea14454b0ebdca90
Ghiles Meddour <ghiles.meddour.b@gmail.com>
2022-05-22 23:41:44 +0200

    Producer: use pydantic Job model

--- src/pyfaktory/producer.py
+++ src/pyfaktory/producer.py
@@ -1,7 +1,10 @@
 import uuid
 from typing import Any, Dict, List, Optional
 
+from pyfaktory.models import Job
+
 from .client import Client
+from .models import Job
 
 
 class Producer:
@@ -13,6 +16,7 @@ class Producer:
     client : pyfaktory.Client
         Faktory client whose `role` must be either 'producer' or 'both'.
     """
+
     def __init__(self, client: Client) -> None:
         if client.role == 'consumer':
             raise ValueError(
@@ -20,77 +24,20 @@ class Producer:
             )
         self.client = client
 
-    def push_job(self,
-                 jobtype: str,
-                 jid: Optional[str] = None,
-                 args: List[Any] = [],
-                 queue: str = 'default',
-                 reserve_for: int = 1800,
-                 at: str = '',
-                 retry: int = 25,
-                 backtrace: int = 5,
-                 custom: Optional[Dict] = None) -> bool:
-        """
-        Push a job to Faktory server.
+    def push(self, job: Job) -> bool:
+        return self.client._push(job.dict())
 
-        Parameters
-        ----------
-        jid : str
-            Globally unique ID for the job.
-        jobtype : str
-            Discriminator used by a worker to decide how to execute a job.
-        args : List[Any]
-            Parameters the worker should use when executing the job.
-        queue : str, default 'default'
-            Which job queue to push this job onto.
-        reserve_for : int, default 1800
-            Number of seconds a job may be held by a worker before
-            it is considered failed (must be greater than 60).
-        at : str, default ''
-            Run the job at approximately this time, immediately if blank.
-            RFC3339 string is expected, but no validation is done client side.
-        retry : int, default 25
-            Number of times to retry this job if it fails.
-            0 discards the failed job, -1 saves the failed
-            job to the dead set.
-        backtrace : int, default 5
-            Number of lines of FAIL information to preserve.
-        custom : dict, default None
-            Provides additional context to the worker executing the job.
+    def push_bulk(self, jobs: List[Job]) -> Dict:
+        return self.client._pushb([j.dict() for j in jobs])
 
-        Returns
-        -------
-        bool
-            Indicates if the push was successful.
-        """
-        if not jid:
-            jid = uuid.uuid4().hex
+    # TODO
+    def batch_new(self):
+        pass
 
-        # Mandatory fields
-        job: Dict[str, Any] = {
-            'jid': jid,
-            'jobtype': jobtype,
-            'args': args,
-        }
+    # TODO
+    def batch_open(self):
+        pass
 
-        if reserve_for < 60:
-            raise ValueError(
-                f'`reserve_for` == {reserve_for}, but must be greater than 60')
-
-        # TODO: add `at` validation
-
-        # Optional fields
-        job.update({
-            'queue': queue,
-            'reserve_for': reserve_for,
-            'at': at,
-            'retry': retry,
-            'backtrace': backtrace,
-            'custom': custom,
-        })
-
-        # Optional fields can be omitted for some values.
-        # TODO: omit fields when possible (this does not change anything,
-        # it just allows to reduce the size of the command to send)
-
-        return self.client._push(job)
+    # TODO
+    def batch_commit(self):
+        pass
