ef2eb82aaae237020bf806eea437404bad7f9b07
Ghiles Meddour <ghiles.meddour.b@gmail.com>
2022-05-25 00:12:41 +0200

    update fproducer example

--- examples/fproducer.py
+++ examples/fproducer.py
@@ -3,7 +3,7 @@ import logging
 import random
 import time
 
-from pyfaktory import Client, Producer
+from pyfaktory import Client, Job, Producer
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     level=logging.DEBUG,
@@ -14,7 +14,8 @@ time.sleep(1)
 with Client(faktory_url='tcp://localhost:7419', role='producer') as client:
     producer = Producer(client=client)
     while True:
-        producer.push_job(jobtype='adder',
-                          args=(random.randint(0,
-                                               1000), random.randint(0, 1000)))
+        job = Job(jobtype='adder',
+                  args=[random.randint(0, 1000),
+                        random.randint(0, 1000)])
+        producer.push(job)
         time.sleep(1)
