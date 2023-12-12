bcac62b202a41447d5e00a28d3cab44594f5777a
Ryan Garver <ryan@sixfold.ai>
2023-12-12 15:04:28 -0800

    Add tests.

--- tests/test_client.py
+++ tests/test_client.py
@@ -20,6 +20,7 @@ class TestClientConstructor:
 
         assert client.host == 'my-server'
         assert client.port == 7419
+        assert client.scheme == 'tcp'
         assert client.password is None
         assert client.role == 'both'
         assert client.timeout == timeout
@@ -35,6 +36,7 @@ class TestClientConstructor:
 
         assert client.host == 'env-server'
         assert client.port == 7400
+        assert client.scheme == 'tcp'
 
     def test_ignore_env_faktory_url(self, monkeypatch):
         env_faktory_url = 'tcp://env-server:7400'
@@ -44,6 +46,7 @@ class TestClientConstructor:
 
         assert client.host == 'arg-server'
         assert client.port == 7411
+        assert client.scheme == 'tcp'
 
     def test_use_default_faktory_url(self, monkeypatch):
         monkeypatch.delenv('FAKTORY_URL', raising=False)
@@ -52,13 +55,22 @@ class TestClientConstructor:
 
         assert client.host == 'localhost'
         assert client.port == 7419
+        assert client.scheme == 'tcp'
 
     def test_set_password(self):
         client = Client(faktory_url='tcp://:sdg145sd@q!@@foo-server:7419')
 
         assert client.host == 'foo-server'
         assert client.port == 7419
+        assert client.scheme == 'tcp'
         assert client.password == 'sdg145sd@q!@'
+    
+    def test_use_tcp_tls(self):
+        client = Client(faktory_url='tcp+tls://foo-server:7419')
+
+        assert client.host == 'foo-server'
+        assert client.port == 7419
+        assert client.scheme == 'tcp+tls'
 
     def test_consumer_specific_fields(self):
         consumer_specific_fields = {
